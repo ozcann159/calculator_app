052d0414be462018a985cc786771036576d99147
Elif Özcan <ozcann159@gmail.com>
2024-07-25 16:30:39 +0300

    Adjust button alignment and style on HomeScreen
    - Fixed button size issue by setting a fixed width
    - Aligned the button to the left using Align widget
    - Added an arrow icon to the button
    - Updated button padding and text style

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool _isMale1 = false;
+  bool _isMale2 = false;
+
   @override
   Widget build(BuildContext context) {
     final double deviceHeight = MediaQuery.of(context).size.height;
@@ -28,94 +31,191 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Color(0xFF458642),
         centerTitle: true,
       ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: deviceHeight / 3.9,
-            backgroundColor: Colors.transparent, // Transparan olarak ayarlanır
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              background: ClipRRect(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30.0),
-                  bottomRight: Radius.circular(30.0),
-                ),
-                child: Container(
-                  color: Color(0xFF458642),
-                  child: Align(
-                    alignment: Alignment.bottomCenter, // Metni aşağı hizalar
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Text(
-                        'Ben bir ...',
-                        style: TextStyle(
-                          fontSize: 40,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
+      body: Column(
+        children: [
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar(
+                  expandedHeight: deviceHeight / 3.9,
+                  backgroundColor:
+                      Colors.transparent, // Transparan olarak ayarlanır
+                  flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    background: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30.0),
+                        bottomRight: Radius.circular(30.0),
+                      ),
+                      child: Container(
+                        color: Color(0xFF458642),
+                        child: Align(
+                          alignment:
+                              Alignment.bottomCenter, // Metni aşağı hizalar
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text(
+                              'Ben bir ...',
+                              style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              SizedBox(
-                height: deviceHeight / 2, // İçeriğin üst kısmına boşluk ekler
-                child: Center(
-                  child: Wrap(
-                    spacing: 50.0, // Aralarındaki boşluk
-                    runSpacing: 50.0, // Satırlar arasındaki boşluk
-                    alignment: WrapAlignment.center,
-                    children: [
-                      Container(
-                        width: 90,
-                        height: 90,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0xFF458642), // Yeşil çerçeve rengi
-                          border: Border.all(
-                            color: Color(0xFF458642), // Çerçeve rengi
-                            width: 4, // Çerçeve kalınlığı
-                          ),
-                        ),
-                        child: ClipOval(
-                          child: Image.asset(
-                            'assets/images/user1.png',
-                            width: 90,
-                            height: 90,
-                            fit: BoxFit.cover,
+                SliverPadding(
+                  padding: EdgeInsets.only(
+                      top: 90.0), // SliverAppBar ile SliverList arasında boşluk
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate([
+                      SizedBox(
+                        height: deviceHeight /
+                            2, // İçeriğin üst kısmına boşluk ekler
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 100,
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Color(
+                                          0xFF458642), // Yeşil çerçeve rengi
+                                      border: Border.all(
+                                        color:
+                                            Color(0xFF458642), // Çerçeve rengi
+                                        width: 4, // Çerçeve kalınlığı
+                                      ),
+                                    ),
+                                    child: ClipOval(
+                                      child: Image.asset(
+                                        'assets/images/user1.png',
+                                        width: 90,
+                                        height: 90,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          8), // Resim ile checkbox arasında boşluk
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text('Kadın'),
+                                      Checkbox(
+                                        value: _isMale1,
+                                        onChanged: (bool? value) {
+                                          setState(() {
+                                            _isMale1 = value!;
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 30), // İki kolon arasında boşluk
+                              Column(
+                                children: [
+                                  Container(
+                                    width: 100,
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Color(
+                                          0xFF458642), // Yeşil çerçeve rengi
+                                      border: Border.all(
+                                        color:
+                                            Color(0xFF458642), // Çerçeve rengi
+                                        width: 4, // Çerçeve kalınlığı
+                                      ),
+                                    ),
+                                    child: ClipOval(
+                                      child: Image.asset(
+                                        'assets/images/user2.png',
+                                        width: 120,
+                                        height: 100,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          8), // Resim ile checkbox arasında boşluk
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text('Erkek'),
+                                      Checkbox(
+                                        value: _isMale2,
+                                        onChanged: (bool? value) {
+                                          setState(() {
+                                            _isMale2 = value!;
+                                          });
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                      Container(
-                        width: 90,
-                        height: 90,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          //color: Color(0xFF458642), // Yeşil çerçeve rengi
-                          border: Border.all(
-                            color: Color(0xFF458642), // Çerçeve rengi
-                            width: 4, // Çerçeve kalınlığı
-                          ),
-                        ),
-                        child: ClipOval(
-                          child: Image.asset(
-                            'assets/images/user2.png',
-                            width: 90,
-                            height: 90,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ],
+                      SizedBox(height: 20), // İçerikle buton arasında boşluk
+                    ]),
                   ),
                 ),
-              ),
-            ]),
+              ],
+            ),
           ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end, // Butonu sola hizalar
+              children: [
+                SizedBox(
+                  width: 200, // Genişliği ihtiyaca göre ayarlayın
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          Color(0xFF458642), // Butonun arka plan rengi
+                      foregroundColor:
+                          Colors.white, // Buton üzerindeki metin rengi
+                      padding: EdgeInsets.symmetric(
+                          vertical: 30.0,
+                          horizontal: 25.0), // Butonun iç padding'i
+                      textStyle: TextStyle(fontSize: 16), // Metin stili
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize
+                          .min, // Buton içeriğine göre boyutlandırır
+                      mainAxisAlignment:
+                          MainAxisAlignment.center, // İçeriği ortalar
+                      children: [
+                        Text('Devam Et'),
+                        SizedBox(width: 8), // Metin ile ikon arasındaki boşluk
+                        Icon(
+                          Icons.arrow_forward,
+                          size: 20,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
